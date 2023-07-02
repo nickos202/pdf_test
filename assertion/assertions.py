54011fc2b774aee1dda9e9070884d38cd318a357
nickos202 <nikolay.sergeevich202@gmail.com>
2023-07-02 23:43:26 +0500

    fix

--- assertion/assertions.py
+++ assertion/assertions.py
@@ -1,6 +1,7 @@
 class Assertions:
 
     """Метод проверки наличия блока с ключем"""
+
     @staticmethod
     def check_block_key(all_block_values, expected_keys):
         for block in all_block_values:
@@ -13,9 +14,9 @@ class Assertions:
     """Метод проверки расположения блоков"""
 
     @staticmethod
-    def check_block_coordinates(all_block_values, expected_keys, expected_coordinate):
+    def check_block_coordinates(all_block_values, expected_keys, coordinates):
         for block in all_block_values:
-            if set(expected_keys).issubset(block[4].keys()) and block[0] == expected_coordinate:
-                print(f"Блок с ключами {expected_keys} имеет координату {expected_coordinate}")
+            if set(expected_keys).issubset(block[4].keys()) and (block[0], block[3]) == coordinates:
+                print(f"Блок с ключами {expected_keys} имеет координаты: {coordinates}")
                 return
-        print(f"Ошибка! Блок с ключами {expected_keys} имеет другие координаты")
+        raise AssertionError(f"Ошибка! Блок с ключами {expected_keys} имеет другие координаты")
--- tests/test_file_pdf.py
+++ tests/test_file_pdf.py
@@ -29,20 +29,23 @@ class Test_PDF:
         Assertions.check_block_key(all_block_values, ['NOTES'])
         Assertions.check_block_key(all_block_values, ['inspection notes'])
 
-    def test_key_line(self):
+    def test_key_coordinate(self):
         all_block_values = ReadPDF.read_pdf()
-        Assertions.check_block_coordinates(all_block_values, ['GRIFFON AVIATION SERVICES LLC'], 6.005304336547852)
-        Assertions.check_block_coordinates(all_block_values, ['PN', 'SN'], 13.511934280395508)
+        Assertions.check_block_coordinates(all_block_values, ['GRIFFON AVIATION SERVICES LLC'], (6.005304336547852,
+                                                                                                 17.99098777770996))
+        Assertions.check_block_coordinates(all_block_values, ['PN', 'SN'], (13.511934280395508, 49.63319396972656))
         Assertions.check_block_coordinates(all_block_values, ['DESCRIPTION', 'LOCATION', 'CONDITION'],
-                                           13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['RECEIVER#', 'UOM'], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['EXP DATE', 'PO'], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['CERT SOURCE', ], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['REC.DATE', 'MFG'], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['BATCH#', 'DOM'], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['REMARK', 'LOT#'], 13.511934280395508)
-        Assertions.check_block_coordinates(all_block_values, ['TAGGED BY'], 13.511934280395508)
-        # Assertions.check_block_coordinates(all_block_values, [], 86.32624816894531)
-        Assertions.check_block_coordinates(all_block_values, ['Qty'], 17.265249252319336)
-        Assertions.check_block_coordinates(all_block_values, ['NOTES'], 141.49998474121094)
-        Assertions.check_block_coordinates(all_block_values, ['inspection notes'], 146.00396728515625)
+                                           (13.511934280395508, 72.15308380126953))
+        Assertions.check_block_coordinates(all_block_values, ['RECEIVER#', 'UOM'], (13.511934280395508,
+                                                                                    83.41303253173828))
+        Assertions.check_block_coordinates(all_block_values, ['EXP DATE', 'PO'], (13.511934280395508, 94.6729736328125))
+        Assertions.check_block_coordinates(all_block_values, ['CERT SOURCE', ], (13.511934280395508, 105.93292236328125))
+        Assertions.check_block_coordinates(all_block_values, ['REC.DATE', 'MFG'], (13.511934280395508, 117.19287109375))
+        Assertions.check_block_coordinates(all_block_values, ['BATCH#', 'DOM'], (13.511934280395508, 128.45281982421875))
+        Assertions.check_block_coordinates(all_block_values, ['REMARK', 'LOT#'], (13.511934280395508, 139.71275329589844))
+        Assertions.check_block_coordinates(all_block_values, ['TAGGED BY'], (13.511934280395508, 150.9727020263672))
+        # Assertions.check_block_coordinates(all_block_values, [], (86.32624816894531, 187.00453186035156))
+        Assertions.check_block_coordinates(all_block_values, ['Qty'], (17.265249252319336, 195.26182556152344))
+        Assertions.check_block_coordinates(all_block_values, ['NOTES'], (141.49998474121094, 150.9727020263672))
+        Assertions.check_block_coordinates(all_block_values, ['inspection notes'], (146.00396728515625,
+                                                                                    163.73397827148438))
