4f9d6f5970a4af7e1df66569e202dc742d6ddfe8
nickos202 <nikolay.sergeevich202@gmail.com>
2023-07-17 17:17:32 +0500

    fix

--- assertion/assertions.py
+++ assertion/assertions.py
@@ -4,9 +4,9 @@ class Assertions:
 
     @staticmethod
     def check_block_key(all_block_values, expected_keys):
-        for block in all_block_values:
-            block_keys = list(block[4].keys())
-            if all(key in block_keys for key in expected_keys):
+        for block in all_block_values: # Проходимся по каждому блоку текста в списке
+            block_keys = list(block[4].keys()) # Извлекаем ключи из каждого блока со словарем ключ:значений
+            if all(key in block_keys for key in expected_keys): # Проверяем наличие всех ожидаемых ключей
                 print(f"Блок с ключами {expected_keys} есть в файле.")
                 return
         raise AssertionError(f"Ошибка! Блок с ключами {expected_keys} не найден.")
@@ -15,7 +15,8 @@ class Assertions:
 
     @staticmethod
     def check_block_coordinates(all_block_values, expected_keys, expected_coordinates):
-        for block in all_block_values:
+        for block in all_block_values: # Проходим по каждому блоку текста в списке
+            # Ищем блок с помощью ключа                          и проверяем, совпадают ли координаты (левая и нижняя)
             if set(expected_keys).issubset(block[4].keys()) and (block[0], block[3]) == expected_coordinates:
                 print(f"Блок с ключами {expected_keys} имеет координаты: {expected_coordinates}")
                 return
